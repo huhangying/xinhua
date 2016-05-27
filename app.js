c61b7bec108ba590e4f2f79419b6d0fde002d8e4
Harry <59187299@qq.com>
2016-05-26 23:54:47 -0400

    changed the response headers

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 
  app.all('*', function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS,PATCH");
    res.header("X-Powered-By",' Harry')
    res.header("Content-Type", "application/json;charset=utf-8");
