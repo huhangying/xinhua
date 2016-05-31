83b40b2395d99a547b440d9dd7b940c8f5fc86f8
Harry <59187299@qq.com>
2016-05-31 02:20:42 -0400

    [chat] fixed bug.

--- db/controller/chat.js
+++ db/controller/chat.js
@@ -15,7 +15,7 @@ module.exports = {
 
 
         // check input(chatroom, direction, type, data)
-        if (!chat.user || !chat.doctor || !chat.direction || !chat.type || !chat.data) {
+        if (!chat.user || !chat.doctor || !chat.data) {
             console.log(JSON.stringify(chat));
             return Status.returnStatus(res, Status.MISSING_PARAM);
         }
