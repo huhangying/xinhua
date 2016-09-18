823eb23dec6cf2868f9435a968e227047f939ef8
Harry <hhu@live.ca>
2016-09-18 11:03:53 -0400

    doctor/relationship: order by not focus doctors

--- db/model/relationship.js
+++ db/model/relationship.js
@@ -16,6 +16,49 @@ var _Relationship = new Schema({
 
 
 var Relationship = mongoose.model('relationship', _Relationship);
+
+
+
+/**
+ * @description
+ * Returns a function which will sort an
+ * array of objects by the given key.
+ *
+ * @param  {String}  key
+ * @param  {Boolean} reverse
+ * @return {Function}
+ */
+function sortBy(key, reverse) {
+
+    // Move smaller items towards the front
+    // or back of the array depending on if
+    // we want to sort the array in reverse
+    // order or not.
+    var moveSmaller = reverse ? 1 : -1;
+
+    // Move larger items towards the front
+    // or back of the array depending on if
+    // we want to sort the array in reverse
+    // order or not.
+    var moveLarger = reverse ? -1 : 1;
+
+    /**
+     * @param  {*} a
+     * @param  {*} b
+     * @return {Number}
+     */
+    return function(a, b) {
+        if (a[key] < b[key]) {
+            return moveSmaller;
+        }
+        if (a[key] > b[key]) {
+            return moveLarger;
+        }
+        return 0;
+    };
+
+}
+
 Relationship.getFocusDoctors = function(userId) {
     var deferred = Q.defer();
 
@@ -30,7 +73,7 @@ Relationship.getFocusDoctors = function(userId) {
 
             }
 
-            var doctors = items.map(function(a) {return a.doctor;});
+            var doctors = items.map(function(a) {return a.doctor;}).sort(sortBy('order'));
             deferred.resolve(doctors);
         });
 
