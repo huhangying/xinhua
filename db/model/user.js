a59ebc8131b4f957fc5cabd90ede51cf91a55ac1
Harry Hu <harryhu@harrys-mac-mini.lan>
2020-01-05 12:37:07 -0500

    update test

--- db/model/user.js
+++ db/model/user.js
@@ -4,31 +4,31 @@
 var Schema = global.mongoose.Schema;
 
 var _User = new Schema({
-    link_id: String,
-    cell: String,
-    name: String,
-    password: String,
-    role: { type: Number, default: 1, min:0, max:1 }, // 0: registered; 1: authorized;
-    created: {type : Date, default: Date.now},
-    updated: {type : Date, default: Date.now},
+  link_id: String,
+  cell: String,
+  name: String,
+  password: String,
+  role: { type: Number, default: 1, min: 0, max: 1 }, // 0: registered; 1: authorized;
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
   associates: [
     {
       hid: { type: String },
       huid: { type: String }
     }
   ],
-    icon: String,
-    gender: String,
-    height: { type: String },
-    weight: { type: String },
-    birthdate: { type: Date },
-    sin: String,
-    admissionNumber: { type: String},
-    visitedDepartments: [
-        { type: Schema.Types.ObjectId, ref: 'department' }
-    ], //用来判定应该使用初诊问卷还是复诊问卷
-    locked_count: Number,
-    apply: {type : Boolean, default: true}
+  icon: String,
+  gender: String,
+  height: { type: String },
+  weight: { type: String },
+  birthdate: { type: Date },
+  sin: String,
+  admissionNumber: { type: String },
+  visitedDepartments: [
+    { type: Schema.Types.ObjectId, ref: 'department' }
+  ], //用来判定应该使用初诊问卷还是复诊问卷
+  locked_count: Number,
+  apply: { type: Boolean, default: true }
 });
 
-module.exports =  mongoose.model('user', _User);
+module.exports = mongoose.model('user', _User);
